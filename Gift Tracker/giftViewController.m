6dc3f196a4670b5d9e2241dc0a90f152a529c201
Tam Do <tamchdo@gmail.com>
2013-07-24 14:05:37 -0700

    done committing and reverting, now I can continue on the gift list

--- Gift Tracker/giftViewController.m
+++ Gift Tracker/giftViewController.m
@@ -30,8 +30,10 @@
 }
 
 - (void) viewDidAppear:(BOOL)animated {
-    self.gifts = [self.dao filterGiftFromSource:self.source withSearchString:self.searchBar.text];
+    [self.navigationController.navigationBar.topItem setRightBarButtonItem:self.navigationItem.rightBarButtonItem];
+    [self.navigationController.navigationBar.topItem setTitle:self.navigationItem.title];
 
+    self.gifts = [self.dao filterGiftFromSource:self.source withSearchString:self.searchBar.text];
     [[self tableView] reloadData];
 }
 
@@ -49,9 +51,16 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"GiftCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    //styling
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
     Gift * g = [gifts objectAtIndex:[indexPath row]];
     cell.textLabel.text = g.description;
+    [cell.detailTextLabel setTextColor:[UIColor colorWithRed:0 green:0.4 blue:0 alpha:1]];
     cell.detailTextLabel.text = [NSString stringWithFormat:@"$%.2f",[g findContributionFromSourceId:self.source.idno].value ] ;
     return cell;
 }
@@ -60,10 +69,15 @@
     return [self.gifts count];
 }
 
-#pragma mark - Table view delegate
+-(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    // DAO takes care of empty search string case too
+    self.gifts = [self.dao filterGiftFromSource:self.source withSearchString:searchText];
+    [self.tableView reloadData];
+}
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-   }
+-(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [searchBar resignFirstResponder];
+}
+
 
 @end
